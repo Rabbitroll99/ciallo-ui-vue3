14db9eaa791e3b571e5fd0ebf6e425b7a11cf911
Rabbitroll99 <Rabbitroll99@163.com>
2025-05-16 06:35:56 +0800

    Button组件现在支持theme属性、size属性、level属性了，并添加了相关样式。

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -7,6 +7,42 @@
     <Button theme="link">你好</Button>
     <Button theme="text">你好</Button>
   </div>
+  <h1>示例2</h1>
+  <div>
+    <div>
+      <Button>普通按钮</Button>
+      <Button size="big">大按钮</Button>
+      <Button size="small">小按钮</Button>
+    </div>
+    <div>
+      <Button theme="link">普通按钮</Button>
+      <Button theme="link" size="big">大按钮</Button>
+      <Button theme="link" size="small">小按钮</Button>
+    </div>
+    <div>
+      <Button theme="text">普通按钮</Button>
+      <Button size="big" theme="text">大按钮</Button>
+      <Button size="small" theme="text">小按钮</Button>
+    </div>
+  </div>
+  <h1>示例3</h1>
+  <div>
+    <dir>
+      <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </dir>
+    <dir>
+      <Button theme="link" level="main">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button theme="link" level="danger">危险链接按钮</Button>
+    </dir>
+    <dir>
+      <Button theme="text" level="main">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button theme="text" level="danger">危险文字按钮</Button>
+    </dir>
+  </div>
 </template>
 <script lang="ts">
 import Button from "../lib/Button.vue";
