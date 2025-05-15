471097a40a8d7bfd8c36d69e89c03e14d33546b5
Rabbitroll99 <Rabbitroll99@163.com>
2025-05-16 05:57:01 +0800

    根据css最小影响原则调整代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,11 @@
 <template>
-  <button @click="toggle" :class="{ checked: value }"><span> </span></button>
+  <button
+    class="ciallo-switch"
+    @click="toggle"
+    :class="{ 'ciallo-checked': value }"
+  >
+    <span> </span>
+  </button>
   <div>{{ value }}</div>
 </template>
 <script lang="ts">
@@ -16,10 +22,10 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
-button {
+.ciallo-switch {
   height: $h;
   width: $h * 2;
   border: none;
@@ -36,7 +42,7 @@ button {
     border-radius: $h2 / 2;
     transition: all 250ms;
   }
-  &.checked {
+  &.ciallo-checked {
     background: #1890ff;
     > span {
       left: calc(100% - #{$h2} - 2px);
@@ -51,7 +57,7 @@ button {
       width: $h2 + 4px;
     }
   }
-  &.checked:active {
+  &.ciallo-checked:active {
     > span {
       width: $h2 + 4px;
       margin-left: -4px;
