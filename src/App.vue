b19ae743e582d61a976e2aba62f38605717bee17
Rabbitroll99 <Rabbitroll99@163.com>
2025-05-16 03:01:26 +0800

    实现了移动端路由切换之后关闭aside的逻辑

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
 <script lang="ts">
 import { provide, ref } from "vue";
+import { router } from "./router";
 
 export default {
   name: "App",
@@ -12,6 +13,11 @@ export default {
     // console.log(width)
     const asideVisible = ref(width <= 500 ? false : true);
     provide("asideVisible", asideVisible); //set
+    router.afterEach(() => {
+      if (width <= 500) {
+        asideVisible.value = false;
+      }
+    });
   },
 };
 </script>
