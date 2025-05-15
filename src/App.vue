cfb446e2ef5c89e093b33ba3a4a476a873ae5c3c
Rabbitroll99 <Rabbitroll99@163.com>
2025-05-16 00:40:40 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <rabbit />
+</template>
+
+<script>
+import rabbit from "./components/rabbit.vue";
+
+export default {
+  name: "App",
+  components: {
+    rabbit,
+  },
+};
+</script>
