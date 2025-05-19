00c5eeeb01dc387473627894be4cd9dfcab2be7c
Rabbitroll99 <Rabbitroll99@163.com>
2025-05-20 05:52:52 +0800

    Dialog现已支持自定义title和content

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -4,12 +4,11 @@
     <div class="ciallo-dialog-wrapper">
       <div class="ciallo-dialog">
         <header>
-          标题
+          <slot name="title" />
           <span @click="close" class="ciallo-dialog-close"></span>
         </header>
         <main>
-          <p>第一行字</p>
-          <p>第二行字</p>
+          <slot name="content" />
         </main>
         <footer>
           <Button level="main" @click="ok">OK</Button>
