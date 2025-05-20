8121f2eeeae66fa41be84b70572f3ecb15b8dc6d
Rabbitroll99 <Rabbitroll99@163.com>
2025-05-21 06:58:08 +0800

    在Diglog组件中使用了Teleport

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,21 +1,23 @@
 <template>
   <template v-if="visible">
-    <div class="ciallo-dialog-overlay" @click="onClickOverlay"></div>
-    <div class="ciallo-dialog-wrapper">
-      <div class="ciallo-dialog">
-        <header>
-          <slot name="title" />
-          <span @click="close" class="ciallo-dialog-close"></span>
-        </header>
-        <main>
-          <slot name="content" />
-        </main>
-        <footer>
-          <Button level="main" @click="ok">OK</Button>
-          <Button @click="cancel">Cancel</Button>
-        </footer>
+    <Teleport to="body">
+      <div class="ciallo-dialog-overlay" @click="onClickOverlay"></div>
+      <div class="ciallo-dialog-wrapper">
+        <div class="ciallo-dialog">
+          <header>
+            <slot name="title" />
+            <span @click="close" class="ciallo-dialog-close"></span>
+          </header>
+          <main>
+            <slot name="content" />
+          </main>
+          <footer>
+            <Button level="main" @click="ok">OK</Button>
+            <Button @click="cancel">Cancel</Button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </Teleport>
   </template>
 </template>
 
