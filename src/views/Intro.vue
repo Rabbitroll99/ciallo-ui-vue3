259a1e1e781b78e3d278e860741a95ed9dbb659d
Rabbitroll99 <Rabbitroll99@163.com>
2025-06-07 18:58:35 +0800

    引入github样式

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>介绍</div>
+  <article class="markdown-body">
+    <h1>介绍</h1>
+    <p>
+      CialloUI 是一个简洁的前端UI框架，基于 Vue3.0 的开源UI组件库，使用
+      vite+vue3.0+typescript
+      搭建，由Rabbitroll99开发，如果你不满原生样式，不必担心类名命名的问题，覆盖样式也是可以的，同时CialloUI还支持移动端的显示。
+    </p>
+    下一节：<a href="#/doc/install">安装</a>
+  </article>
 </template>
