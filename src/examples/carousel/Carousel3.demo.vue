ad8a191ae8945f6874f92baf8335e7d240afe03d
Rabbitroll99 <Rabbitroll99@163.com>
2025-06-12 17:57:19 +0800

    修改了图片路径

--- src/examples/carousel/Carousel3.demo.vue
+++ src/examples/carousel/Carousel3.demo.vue
@@ -16,23 +16,23 @@ import { Carousel } from "../../lib/index";
 
 const images = [
     {
-        src: '/img/Rotating-demo-image1.png',
+        src: './img/Rotating-demo-image1.png',
         alt: '图片1'
     },
     {
-        src: '/img/Rotating-demo-image2.png',
+        src: './img/Rotating-demo-image2.png',
         alt: '图片2'
     },
     {
-        src: '/img/Rotating-demo-image3.png',
+        src: './img/Rotating-demo-image3.png',
         alt: '图片3'
     },
     {
-        src: '/img/Rotating-demo-image4.png',
+        src: './img/Rotating-demo-image4.png',
         alt: '图片4'
     },
     {
-        src: '/img/Rotating-demo-image5.png',
+        src: './img/Rotating-demo-image5.png',
         alt: '图片5'
     }
 ]
--- src/examples/carousel/Carousel5.demo.vue
+++ src/examples/carousel/Carousel5.demo.vue
@@ -16,31 +16,31 @@ import { Carousel } from "../../lib/index";
 
 const images = [
     {
-        src: '/img/Rotating-demo-image1.png',
+        src: './img/Rotating-demo-image1.png',
         alt: '图片1'
     },
     {
-        src: '/img/Rotating-demo-image2.png',
+        src: './img/Rotating-demo-image2.png',
         alt: '图片2'
     },
     {
-        src: '/img/Rotating-demo-image3.png',
+        src: './img/Rotating-demo-image3.png',
         alt: '图片3'
     },
     {
-        src: '/img/Rotating-demo-image4.png',
+        src: './img/Rotating-demo-image4.png',
         alt: '图片4'
     },
     {
-        src: '/img/Rotating-demo-image5.png',
+        src: './img/Rotating-demo-image5.png',
         alt: '图片5'
     },
     {
-        src: '/img/Rotating-demo-image6.png',
+        src: './img/Rotating-demo-image6.png',
         alt: '图片6'
     },
     {
-        src: '/img/Rotating-demo-image7.png',
+        src: './img/Rotating-demo-image7.png',
         alt: '图片7'
     }
 ]
